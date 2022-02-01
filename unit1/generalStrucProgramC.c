72bcdf81a96c589b48102f459492f31e3de24ddc
FatimaMirandap <mirandapmx@gmail.com>
2022-02-01 00:55:40 -0600

    estructura general C

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//* Global Variables *//;
+//Create a variable: dataType nameVariable = anyData; 
+//dataType in C/C++/C#/PHP
+int unEntero = 3;
+int unDecimal = 3.1416;
+char* name = "FatimaMirandap";
+char unCaracter = 'F';
+////////////////////////////////////
+
+
+/// La ejecucion del programa//
+int main(){
+    printf("Hello Data B %s\n", name);
+    return 0;
+}
+//////////////////////////////
