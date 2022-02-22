b48d8bb6c248de656654e59dba01d75bd25cfd1f
FatimaMirandap <mirandapmx@gmail.com>
2022-02-22 10:30:52 -0600

    evenOddExample

--- unit1/operadoresCondiciones.c
+++ unit1/operadoresCondiciones.c
@@ -14,8 +14,8 @@ int sofa = 7000;
 int main(int agrc, char** argv){
     int inputTemp = atoi (argv [1]);
     int inputMoney = atoi (argv[2]);
-    char inputName = argv[3][0]; //--> "Female"---> {'F', 'e', 'm', 'a', 'l', 'e','\0'}
-    int temp_condition = ( inputTemp == frio );
+    char inputName = argv[3][0]; //--> "Female"---> {'F', 'e', 'm', 'a', 'l', 'e','\0'};
+    int temp_condition = (inputTemp == frio);
     int temp_condition2 = (inputTemp == caliente);
     int temp_condition3 = (inputTemp >= caliente);
     int canRobotMove = (inputTemp <= caliente);
@@ -23,15 +23,15 @@ int main(int agrc, char** argv){
     int isUserFemale = (inputName != 'F');
 
 
-    if(canRobotMove == true){
+    if(canRobotMove)}
         //Robot.move(x_coord, y_coord);
     } else {
         //Robot.stop(x_coord, y_coord);
     }
-if(hasUserMoney){
+    if(hasUserMoney){
     //Allows to buy
-}
-else{
+    }
+    else{
     //rejected product
 }
 
